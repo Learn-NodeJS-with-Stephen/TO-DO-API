43bcb1743bf2ed907bc548ac83ca834a2552298f
Isreal Iyaji <isrealiyaji1@gmail.com>
2025-01-18 13:14:01 +0100

    added CRUD ops to the TODO

--- routes/tasksRoute.js
+++ routes/tasksRoute.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const tasksController = require('../controllers/tasksController')
-
+const tasksController = require("../controllers/tasksController");
 //index.js -> tasksRoute -> tasksCOntroller
 
-
-router.post('/', tasksController.createTask);
-router.get('/', tasksController.getAllTasks);
+router.post("/", tasksController.createTask);
+router.get("/", tasksController.getAllTasks);
+router.get("/unfinished", tasksController.getallUnfinishedTasks);
+router.get("/completed", tasksController.getAllCompletedTasks);
+router.get("/:taskId", tasksController.getSingleTask);
+// router.put("/:taskId", tasksController.updateTask);
+router.put("/:taskId", tasksController.updateTask);
+router.delete("/completed", tasksController.deleteAllCompletedTask);
+router.delete("/:taskId", tasksController.deleteTask);
+router.delete("/", tasksController.deleteAllTask);
 // router.get('/completed', tasksController.getAllTasks);
 
-module.exports = router
+module.exports = router;
